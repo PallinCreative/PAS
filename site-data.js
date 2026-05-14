586593bb5e54b4b79f2dac893f096972b912862e
PallinCreative <kriss.pallin@gmail.com>
2026-05-14 11:05:59 +0200

    Create site-data.js
    Nettstedsdata

---
+++ site-data.js
@@ -0,0 +1,60 @@
+const PAS_DATA = [
+    {
+        id: "relasjoner",
+        mainLabel: "Mellommenneskelige relasjoner",
+        descLabel: "Hvem liker jeg å være sammen med & hva kan vi finne på sammen",
+        colorMain: "#6A2B86", colorDesc: "#6A2B86",
+        link: "relasjoner.html", 
+        slices: [
+            { label: "Min familie", color: "#936AB0", link: "familie.html" },
+            { label: "Mine venner", color: "#A985C1", link: "venner.html" },
+            { label: "Mine hjelpere", color: "#C4ACD7", link: "hjelpere.html" },
+            { label: "Lokal-|samfunnet|mitt", color: "#E5DAED", link: "lokalsamfunn.html" }
+        ]
+    },
+    {
+        id: "omgivelser",
+        mainLabel: "Fysiske omgivelser",
+        descLabel: "Steder jeg liker å være & hvordan jeg liker at de skal være",
+        colorMain: "#00747A", colorDesc: "#00747A",
+        link: "omgivelser.html",
+        slices: [
+            { label: "Hjemmet|mitt", color: "#009FA5", link: "hjemmet.html" },
+            { label: "Steder|utenfor|hjemmet mitt", color: "#76CDD0", link: "steder-utenfor.html" },
+            { label: "Personalets|ferdigheter|& kunnskaper", color: "#D5EFEF", link: "personale.html" }
+        ]
+    },
+    {
+        id: "tid",
+        mainLabel: "Meningsfull tid",
+        descLabel: "Ting jeg liker å gjøre & planer for ukene som kommer",
+        colorMain: "#2B5592", colorDesc: "#2B5592",
+        link: "tid.html",
+        slices: [
+            { label: "Mitt arbeid", color: "#678EC9", link: "arbeid.html" },
+            { label: "Fritid og|moro", color: "#A2C0E2", link: "fritid.html" },
+            { label: "Mine|aktiviteter", color: "#D9E8F5", link: "aktiviteter.html" }
+        ]
+    },
+    {
+        id: "utvikling",
+        mainLabel: "Personlig utvikling",
+        descLabel: "Lære nye ferdigheter",
+        colorMain: "#A9225E", colorDesc: "#A9225E",
+        link: "utvikling.html",
+        slices: [
+            { label: "Kommunikasjons-|ferdigheter", color: "#D97CA4", link: "kommunikasjon.html" },
+            { label: "Lære nye|ferdigheter", color: "#F2CADB", link: "ferdigheter.html" }
+        ]
+    },
+    {
+        id: "helse",
+        mainLabel: "Helse",
+        descLabel: "Best mulig helse for meg",
+        colorMain: "#727679", colorDesc: "#727679",
+        link: "helse.html",
+        slices: [
+            { label: "Best mulig|helse", color: "#B5B7B9", link: "helse.html" }
+        ]
+    }
+];
